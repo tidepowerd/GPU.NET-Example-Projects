193e66bac1f67c504f5861ecef11e012b0e2a8da
Jack Pappas <jack.pappas+github@tidepowerd.com>
2010-11-12 18:52:13 -0500

    - Adding C# Black-Scholes option pricing demo

---
+++ CSharp.BlackScholes/BlackScholes.cs
@@ -0,0 +1,140 @@
+﻿// Copyright 2010 -- TidePowerd, Ltd. All rights reserved.
+// http://www.tidepowerd.com
+//
+// GPU.NET Black-Scholes Demo Application
+// Modified: 6-Aug-2010
+
+using System;
+using TidePowerd.DeviceMethods;
+
+namespace TidePowerd.Demo.CSharp.BlackScholes
+{
+    /// <summary>
+    /// Contains method to calculate option prices via the Black-Scholes formula.
+    /// </summary>
+    internal static class BlackScholes
+    {
+        #region Methods
+
+        /// <summary>
+        /// Computes the values of call and put options using the Black-Scholes formula.
+        /// </summary>
+        /// <param name="callResult">The array in which the call option prices are to be stored.</param>
+        /// <param name="putResult">The array in which the put option prices are to be stored.</param>
+        /// <param name="stockPrice">Contains stock prices.</param>
+        /// <param name="optionStrike">Contains the strike prices of the call/put options.</param>
+        /// <param name="optionYears">The time-to-expiration of the options, in years.</param>
+        /// <param name="riskFree">The risk-free interest rate.</param>
+        /// <param name="volatility">The volatility of the underlying stock.</param>
+        /// <remarks>
+        /// All of the parameter arrays should have the same length.
+        /// </remarks>
+        [KernelMethod(AllowFloatingPointDemotion = true)]
+        public static void BlackScholesGPU(float[] callResult, float[] putResult, float[] stockPrice, float[] optionStrike, float[] optionYears, float riskFree, float volatility)
+        {
+            // Declare constants
+            const float A1 = 0.31938153f;
+            const float A2 = -0.356563782f;
+            const float A3 = 1.781477937f;
+            const float A4 = -1.821255978f;
+            const float A5 = 1.330274429f;
+            const float RSQRT2PI = 0.39894228040143267793994605993438f;
+
+            // Thread index
+            int ThreadId = BlockDimension.X * BlockIndex.X + ThreadIndex.X;
+
+            // Total number of threads in execution grid
+            int TotalThreads = BlockDimension.X * GridDimension.X;
+
+            // No matter how small execution grid is, or how many options we're processing,
+            // by using this loop we'll get perfect memory coalescing
+            for (int OptionIndex = ThreadId; OptionIndex < callResult.Length; OptionIndex += TotalThreads)
+            {
+                float s = stockPrice[OptionIndex];
+                float x = optionStrike[OptionIndex];
+                float t = optionYears[OptionIndex];
+
+                // Calculate the square root of the time to option expiration, in years
+                float SqrtT = DeviceMath.Sqrt(t);
+
+                // Calculate the Black-Scholes parameters
+                float d1 = (DeviceMath.Log(s / x) + (riskFree + 0.5f * volatility * volatility) * t) / (volatility * SqrtT);
+                float d2 = d1 - volatility * SqrtT;
+
+                // Plug the parameters into the Cumulative Normal Distribution (CND)
+                float K1 = 1.0f / (1.0f + 0.2316419f * DeviceMath.Abs(d1));
+                float CndD1 = RSQRT2PI * DeviceMath.Exp(-0.5f * d1 * d1) *
+                    (K1 * (A1 + K1 * (A2 + K1 * (A3 + K1 * (A4 + K1 * A5)))));
+                if (d1 > 0) { CndD1 = 1.0f - CndD1; }
+
+                float K2 = 1.0f / (1.0f + 0.2316419f * DeviceMath.Abs(d2));
+                float CndD2 = RSQRT2PI * DeviceMath.Exp(-0.5f * d2 * d2) *
+                    (K2 * (A1 + K2 * (A2 + K2 * (A3 + K2 * (A4 + K2 * A5)))));
+                if (d2 > 0) { CndD2 = 1.0f - CndD2; }
+
+                // Calculate the discount rate
+                float ExpRT = DeviceMath.Exp(-1.0f * riskFree * t);
+
+                // Calculate the values of the call and put options
+                callResult[OptionIndex] = s * CndD1 - x * ExpRT * CndD2;
+                putResult[OptionIndex] = x * ExpRT * (1.0f - CndD2) - s * (1.0f - CndD1);
+            }
+        }
+
+        /// <summary>
+        /// Computes the values of call and put options using the Black-Scholes formula.
+        /// </summary>
+        /// <param name="callResult">The array in which the call option prices are to be stored.</param>
+        /// <param name="putResult">The array in which the put option prices are to be stored.</param>
+        /// <param name="stockPrice">Contains stock prices.</param>
+        /// <param name="optionStrike">Contains the strike prices of the call/put options.</param>
+        /// <param name="optionYears">The time-to-expiration of the options, in years.</param>
+        /// <param name="riskFree">The risk-free interest rate.</param>
+        /// <param name="volatility">The volatility of the underlying stock.</param>
+        /// <remarks>
+        /// All of the parameter arrays should have the same length.
+        /// </remarks>
+        public static void BlackScholesCPU(float[] callResult, float[] putResult, float[] stockPrice, float[] optionStrike, float[] optionYears, float riskFree, float volatility)
+        {
+            // Declare constants
+            const float A1 = 0.31938153f;
+            const float A2 = -0.356563782f;
+            const float A3 = 1.781477937f;
+            const float A4 = -1.821255978f;
+            const float A5 = 1.330274429f;
+            const float RSQRT2PI = 0.39894228040143267793994605993438f;
+
+            for (int OptionIndex = 0; OptionIndex < callResult.Length; OptionIndex++)
+            {
+                float s = stockPrice[OptionIndex];
+                float x = optionStrike[OptionIndex];
+                float t = optionYears[OptionIndex];
+
+                // Calculate the square root of the time to option expiration, in years
+                float SqrtT = (float)Math.Sqrt(t);
+
+                // Calculate the Black-Scholes parameters
+                float d1 = ((float)Math.Log(s / x) + (riskFree + 0.5f * volatility * volatility) * t) / (volatility * SqrtT);
+                float d2 = d1 - volatility * SqrtT;
+
+                // Plug the parameters into the Cumulative Normal Distribution (CND)
+                float K1 = 1.0f / (1.0f + 0.2316419f * Math.Abs(d1));
+                float CndD1 = RSQRT2PI * (float)Math.Exp(-0.5f * d1 * d1) * (K1 * (A1 + K1 * (A2 + K1 * (A3 + K1 * (A4 + K1 * A5)))));
+                if (d1 > 0) { CndD1 = 1.0f - CndD1; }
+
+                float K2 = 1.0f / (1.0f + 0.2316419f * Math.Abs(d2));
+                float CndD2 = RSQRT2PI * (float)Math.Exp(-0.5f * d2 * d2) * (K2 * (A1 + K2 * (A2 + K2 * (A3 + K2 * (A4 + K2 * A5)))));
+                if (d2 > 0) { CndD2 = 1.0f - CndD2; }
+
+                // Calculate the discount rate
+                float ExpRT = (float)Math.Exp(-1.0f * riskFree * t);
+
+                // Calculate the values of the call and put options
+                callResult[OptionIndex] = s * CndD1 - x * ExpRT * CndD2;
+                putResult[OptionIndex] = x * ExpRT * (1.0f - CndD2) - s * (1.0f - CndD1);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ CSharp.BlackScholes/Program.cs
@@ -0,0 +1,166 @@
+﻿// Copyright 2010 -- TidePowerd, Ltd. All rights reserved.
+// http://www.tidepowerd.com
+//
+// GPU.NET Black-Scholes Demo Application
+// Modified: 6-Aug-2010
+
+using System;
+using System.Diagnostics;
+
+using TidePowerd.DeviceMethods;
+
+namespace TidePowerd.Demo.CSharp.BlackScholes
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Set the number of options and iterations
+            const int NumOptions = 4000000;
+            const int NumGPUIterations = 40;
+            const int NumCPUIterations = 1;
+
+            // Set some simulation parameters
+            const float RiskFree = 0.02f;
+            const float Volatility = 0.30f;
+
+            // Allocate arrays to hold option data and pricing results
+            float[] StockPrices = new float[NumOptions];
+            float[] OptionStrikePrices = new float[NumOptions];
+            float[] OptionYears = new float[NumOptions];
+
+            // Generate random option data
+            Random rand = new Random();
+            for (int i = 0; i < NumOptions; i++)
+            {
+                StockPrices[i] = RandFloat(rand, 5.0f, 30.0f);
+                OptionStrikePrices[i] = RandFloat(rand, 1.0f, 100.0f);
+                OptionYears[i] = RandFloat(rand, 0.25f, 10.0f);
+            }
+
+            // Create copies of the option data arrays for use with the CPU calculations
+            float[] CallResultsCPU = new float[NumOptions];
+            float[] PutResultsCPU = new float[NumOptions];
+            float[] StockPricesCPU = new float[NumOptions];
+            Array.Copy(StockPrices, StockPricesCPU, NumOptions);
+            float[] OptionStrikePricesCPU = new float[NumOptions];
+            Array.Copy(OptionStrikePrices, OptionStrikePricesCPU, NumOptions);
+            float[] OptionYearsCPU = new float[NumOptions];
+            Array.Copy(OptionYears, OptionYearsCPU, NumOptions);
+
+            // Create copies of the option data arrays for use with the GPU calculations
+            float[] CallResultsGPU = new float[NumOptions];
+            float[] PutResultsGPU = new float[NumOptions];
+            float[] StockPricesGPU = new float[NumOptions];
+            Array.Copy(StockPrices, StockPricesGPU, NumOptions);
+            float[] OptionStrikePricesGPU = new float[NumOptions];
+            Array.Copy(OptionStrikePrices, OptionStrikePricesGPU, NumOptions);
+            float[] OptionYearsGPU = new float[NumOptions];
+            Array.Copy(OptionYears, OptionYearsGPU, NumOptions);
+
+            // Set grid/block size for GPU execution
+            Launcher.SetGridSize(480);
+            Launcher.SetBlockSize(128);
+
+            // Begin thread and processor affinity so we don't jump to a different core and skew our timing results
+            System.Threading.Thread.BeginThreadAffinity();
+            // TODO: Set processor affinity mask
+
+            // Print message to console
+            Console.Write("Performing GPU-based calculations...");
+
+            // Create and start stopwatch for performance calculations
+            Stopwatch Watch = Stopwatch.StartNew();
+
+            // Execute the kernel "NumIterations" times
+            for (int Iteration = 0; Iteration < NumGPUIterations; Iteration++)
+            {
+                BlackScholes.BlackScholesGPU(CallResultsGPU, PutResultsGPU, StockPricesGPU, OptionStrikePricesGPU, OptionYearsGPU, RiskFree, Volatility);
+            }
+
+            // Stop the stopwatch and print GPU timing results
+            Watch.Stop();
+            double ElapsedMillisecondsPerGPUIteration = Watch.Elapsed.TotalMilliseconds / (double)NumGPUIterations;
+            Console.WriteLine("Completed {0} iterations in {1:0.0000} ms.", NumGPUIterations, Watch.Elapsed.TotalMilliseconds);
+            Console.WriteLine("Average time per iteration: {0:0.0000} ms.", ElapsedMillisecondsPerGPUIteration);
+            Console.WriteLine();
+
+            // Print message to console
+            Console.WriteLine("Performing CPU-based calculations...");
+
+            // Restart the stopwatch
+            Watch.Reset();
+            Watch.Start();
+
+            // Perform CPU-based calculations
+            for (int Iteration = 0; Iteration < NumCPUIterations; Iteration++)
+            {
+                BlackScholes.BlackScholesCPU(CallResultsCPU, PutResultsCPU, StockPricesCPU, OptionStrikePricesCPU, OptionYearsCPU, RiskFree, Volatility);
+            }
+
+            // Stop the stopwatch and end thread affinity
+            Watch.Stop();
+            double ElapsedMillisecondsPerCPUIteration = Watch.Elapsed.TotalMilliseconds / (double)NumCPUIterations;
+            System.Threading.Thread.EndThreadAffinity();
+
+            // Print CPU timing results
+            Console.WriteLine("Completed {0} iterations in {1:0.0000} ms.", NumCPUIterations, Watch.Elapsed.TotalMilliseconds);
+            Console.WriteLine("Average time per iteration: {0:0.0000} ms.", ElapsedMillisecondsPerCPUIteration);
+            Console.WriteLine();
+
+            // Print performance comparison data
+            Console.WriteLine("Option Count: {0}", 2 * NumOptions);
+            double GibibytesTransferred  = (5.0d * (double)NumOptions * (double)sizeof(float) * 2.0d) / Math.Pow(2.0d, 30.0d);  // GiB transferred (per round-trip)
+            Console.WriteLine("Effective Memory Bandwidth (avg): {0} GiB/s", GibibytesTransferred / (ElapsedMillisecondsPerGPUIteration / 1000.0d));  // GiB transferred (round trip) per iteration / seconds per iteration
+            Console.WriteLine("GPU Speedup vs. CPU: ~{0}x", ElapsedMillisecondsPerCPUIteration / ElapsedMillisecondsPerGPUIteration);
+            Console.WriteLine();
+
+            // Print message to console
+            Console.Write("Verifying calculations...");
+
+            // Verify that GPU & CPU calculations match (their difference should be within a certain threshold)
+            double SumDelta = 0d, SumRef = 0d, MaxDelta = 0d;
+            for (int OptionIndex = 0; OptionIndex < CallResultsCPU.Length; OptionIndex++)
+            {
+                double Delta = Math.Abs(CallResultsCPU[OptionIndex] - CallResultsGPU[OptionIndex]);
+                
+                if (Delta > MaxDelta) { MaxDelta = Delta; }
+
+                SumDelta += Delta;
+
+                SumRef = Math.Abs(CallResultsCPU[OptionIndex]);
+            }
+            double L1Norm = SumDelta / SumRef;
+
+            // Print verification message to console
+            Console.WriteLine("L1 Norm: {0}", L1Norm);
+            Console.WriteLine("Max absolute error: {0}", MaxDelta);
+
+            //// The maximum L1 Norm between GPU and CPU result value
+            ////const float MaxL1NormError = 0.000001f;       // 1E-6 or 0.0001%
+            //const float MaxL1NormError = 0.001f;       // 1E-3 or 0.1%
+            
+            //// Print a message denoting if verification has passed or not
+            //Console.WriteLine((L1Norm < MaxL1NormError) ? "Verification successful!" : "Verification ERROR!");
+
+            // Wait to exit
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            ConsoleKeyInfo cki = Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Generates a random floating point number within a specified range.
+        /// </summary>
+        /// <param name="rand"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        /// <returns></returns>
+        private static float RandFloat(Random rand, float low, float high)
+        {
+            float t = (float)rand.NextDouble();
+            return (1.0f - t) * low + t * high;
+        }
+    }
+}
